8ec2245581c676d911a9f2d03f47a2a18063f64c
Chiucchi <gchiucchi01@gmail.com>
2021-06-28 09:57:11 -0300

    Aula 5

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -9,6 +9,8 @@ import { Question } from '../components/Question';
 import { RoomCode } from '../components/RoomCode';
 
 import logoImg from '../assets/images/logo.svg';
+import checkImg from '../assets/images/check.svg';
+import answerImg from '../assets/images/answer.svg';
 import deleteImg from '../assets/images/delete.svg';
 import '../styles/room.scss';
 
@@ -40,6 +42,19 @@ export function AdminRoom() {
 		}
 	}
 
+	async function handleCheckQuestionAsAnswered(questionId: string) {
+		await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+			isAnswered: true,
+			isHighlighted: false,
+		});
+	}
+
+	async function handleHighlightQuestion(questionId: string) {
+		await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+			isHighlighted: true,
+		});
+	}
+
 	return (
 		<div id='page-room'>
 			<header>
@@ -69,7 +84,33 @@ export function AdminRoom() {
 								key={question.id}
 								content={question.content}
 								author={question.author}
+								isAnswered={question.isAnswered}
+								isHighlighted={question.isHighlighted}
 							>
+								{!question.isAnswered && (
+									<>
+										<button
+											type='button'
+											onClick={() =>
+												handleCheckQuestionAsAnswered(question.id)
+											}
+										>
+											<img src={checkImg} alt='marcar pergunta' />
+										</button>
+										<button
+											type='button'
+											onClick={() =>
+												handleHighlightQuestion(question.id)
+											}
+										>
+											<img
+												src={answerImg}
+												alt='dar destaque na pergunta'
+											/>
+										</button>
+									</>
+								)}
+
 								<button
 									type='button'
 									onClick={() => handleDeleteQuestion(question.id)}
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -2,64 +2,65 @@ import { useAuth } from '../hooks/useAuth';
 import { Link, useHistory } from 'react-router-dom';
 import { FormEvent, useState } from 'react';
 
-import illustrationImg from '../assets/images/illustration.svg'
-import logoImg from '../assets/images/logo.svg'
-import googleIconImg from '../assets/images/google-icon.svg'
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
 
 import { Button } from '../components/Button';
 
-import '../styles/auth.scss'
+import '../styles/auth.scss';
 import { database } from '../services/firebase';
 
-export function NewRoom () {
-   const { user } = useAuth();
-   const history = useHistory();
-   const [ newRoom, setNewRoom ] = useState('');
+export function NewRoom() {
+	const { user } = useAuth();
+	const history = useHistory();
+	const [newRoom, setNewRoom] = useState('');
 
-   async function handleCreateRoom(event: FormEvent) {
-      event.preventDefault();
+	async function handleCreateRoom(event: FormEvent) {
+		event.preventDefault();
 
-      if (newRoom.trim() === '') {
-         return;
-      }
+		if (newRoom.trim() === '') {
+			return;
+		}
 
-      const roomRef = database.ref('rooms');
+		const roomRef = database.ref('rooms');
 
-      const firebaseRoom = await roomRef.push({
-         title: newRoom,
-         authorId: user?.id,
-      });
+		const firebaseRoom = await roomRef.push({
+			title: newRoom,
+			authorId: user?.id,
+		});
 
-      history.push(`/rooms/${firebaseRoom.key}`)
-   }
+		history.push(`/rooms/${firebaseRoom.key}`);
+	}
 
-   
-
-   return(
-      <div id="page-auth">
-         <aside>
-            <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
-            <strong>Crie salas de Q&amp;A ao-vivo</strong>
-            <p>Tire as dúvidas de sua audiência em tempo real</p>
-         </aside>
-         <main>
-            <div className="main-content">
-               <img src={logoImg} alt="Letmeask" />
-               <h2>Criar uma nova sala</h2>
-               <form onSubmit={handleCreateRoom}>
-                  <input 
-                     type="text" 
-                     value={newRoom}
-                     placeholder="Nome da sala"
-                     onChange={event => setNewRoom(event.target.value)}
-                  />
-                  <Button type="submit">
-                     Criar sala
-                  </Button>
-               </form>
-               <p>Quer entrar em uma sala existente? <Link to="/">clique aqui</Link></p>
-            </div>
-         </main>
-      </div>
-   )
+	return (
+		<div id='page-auth'>
+			<aside>
+				<img
+					src={illustrationImg}
+					alt='Ilustração simbolizando perguntas e respostas'
+				/>
+				<strong>Crie salas de Q&amp;A ao-vivo</strong>
+				<p>Tire as dúvidas de sua audiência em tempo real</p>
+			</aside>
+			<main>
+				<div className='main-content'>
+					<img src={logoImg} alt='Letmeask' />
+					<h2>Criar uma nova sala</h2>
+					<form onSubmit={handleCreateRoom}>
+						<input
+							type='text'
+							value={newRoom}
+							placeholder='Nome da sala'
+							onChange={(event) => setNewRoom(event.target.value)}
+						/>
+						<Button type='submit'>Criar sala</Button>
+					</form>
+					<p>
+						Quer entrar em uma sala existente?{' '}
+						<Link to='/'>clique aqui</Link>
+					</p>
+				</div>
+			</main>
+		</div>
+	);
 }
